856666a388c4d4828ca84a53a38cabc792c0cf57
Farronilucas <farronilucas@hotmail.fr>
2022-05-13 14:44:44 +0200

    smooth defilement

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 };
 
     document.getElementById("defilement_droite").onclick = function(){
-    document.getElementById("div_avec_scroll").scrollLeft += 250;
+    document.getElementById("div_avec_scroll").scrollLeft += 250 ;
 };
 
