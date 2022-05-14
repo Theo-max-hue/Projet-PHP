4e2052c58cdeb7df0e6fed449e3d7b3a01fef83c
Farronilucas <farronilucas@hotmail.fr>
2022-05-14 12:36:47 +0200

    style profil

--- profil.php
+++ profil.php
@@ -52,14 +52,9 @@ if (isset($valider)) {
 </script>
 
 
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
+<?php
+require("header.php");
+?>
 
 <body onLoad="document.formProfil.nom.focus()">
 
@@ -95,3 +90,6 @@ if (isset($valider)) {
     </form>
 
 </body>
+<?php
+require("footer.php");
+?>
